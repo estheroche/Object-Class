32e17c97bfb58d0c8646d430efc3907f2ac36ed7
estheroche <estheraladioche569@gmail.com>
2023-02-13 15:31:31 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const house = {
+    window: "Glass",
+    type: "Bungalow",
+    color: "Green",
+    door: "Wooden"
+}
+
+
+let complimentColor = house.color;
+let message = `The trending type of house is ${house.type} a ${house.door}, ${house.color}, in color, with a new modern window made of ${house.window} was built.`;
+alert(message);
